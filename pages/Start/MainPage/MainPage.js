7d6e6ecdefe8b77e3a036cfad0365b58ee10e147
fgh <you@example.com>
2016-11-18 15:45:16 +0800

    Finish MainPage

--- pages/Start/MainPage/MainPage.js
+++ pages/Start/MainPage/MainPage.js
@@ -1,12 +1,15 @@
+var pageData = {},
+    type = [
+        'view', 'content', 'form', 'interact', 'nav', 'media', 'map', 'canvas'
+    ];
 
-Page({
-  data: {
-    films: [],
-    hasMore: true,
-    showLoading: true,
-    start: 0
-  },
-    DirectToJoin: function() {
-     console.log('Join')
+pageData.widgetsToggle = function (e) {
+    var id = e.currentTarget.id, data = {};
+    for (var i = 0, len = type.length; i < len; ++i) {
+        data[type[i] + 'Show'] = false;
     }
-})
+    data[id + 'Show'] = !this.data[id + 'Show'];
+    this.setData(data);
+};
+//使用函数操纵是否显示下拉框
+Page(pageData);
