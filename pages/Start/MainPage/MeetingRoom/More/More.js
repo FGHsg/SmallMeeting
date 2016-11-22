8c5e74b61e5a5601ac1a56135b3737d46ef02325
cargel_ <cargel@sina.com>
2016-11-22 15:08:34 +0800

    yamaidie

--- pages/Start/MainPage/MeetingRoom/More/More.js
+++ pages/Start/MainPage/MeetingRoom/More/More.js
@@ -1,41 +1,47 @@
+Page({
+  data: {
+    text: "Page Showing"
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  },
+  navigateToShow: function () {
+    wx.navigateTo({ url: '../Showing/Showing' })
+  },
+  navigateToParticipator: function () {
+    wx.navigateTo({ url: '../Participator/Participator' })
+  },
+  navigateToMore: function () {
+    wx.navigateTo({ url: '../More/More' })
+  },
+  openFile: function () {
+    wx.downloadFile({
+      url: 'http://www.ghplay.cn/PLAY.HTML',
+      fail: function (res) {
+        console.log('打开文档失败')
+      },
+      success: function (res) {
+        var filePath = res.tempFilePath
+        wx.openDocument({
+          filePath: filePath,
+          success: function (res) {
+            console.log('打开文档成功')
+          }
+        })
+      }
 
-.weui-tabbar {
-    display: flex;
-    position: absolute;
-    z-index: 500;
-    bottom: 0;
-    width: 100%;
-    background-color: black;
-}
-
-.textStyle {
-  display: flex;
-  padding-top:0px;  
-  flex-direction: row;
-  justify-content: flex-end;
-  background-color: black;
-  color: #eb4849;
-  font-family: 'Microsoft Yahei','Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-}
-.weui-tabbar__item {
-    display:flex;
-    flex: 1;
-    justify-content: center;
-    padding: 7px 0 0;
-    font-size: 0;
-    color: white;
-    
-}
-.weui-tabbar__icon {
-    display: flex;
-    width: 24px;
-    height: 24px;
-}
-
-.weui-tabbar__label {
-    text-align: center;
-    font-size: 12px;
-    display:flex;
-    flex: 1;
-    font-family: 'Microsoft Yahei','Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-}
+    })
+  }
+})
